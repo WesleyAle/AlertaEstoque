adcfe51a3a7855959ab23b29e4cecd0079294885
Wesley Alexandre <wesleygamersz@hotmail.com>
2018-04-24 08:26:30 -0300

    Estoque

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace AlertaEstoque
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+             double qtdMaxima, qtAtual, resultado;
+            string nomeProduto;
+            Console.Clear();
+
+            Console.WriteLine("Digite o nome do produto");
+            nomeProduto = Console.ReadLine();
+
+            Console.WriteLine("Digite a quantidade atual do produto");
+            qtAtual = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Digite a quantidade máxima do produto");
+            qtdMaxima = double.Parse(Console.ReadLine());
+
+            resultado = (qtAtual / qtdMaxima)*100;
+
+            Console.WriteLine(resultado.ToString("n2")+"%");
+
+            if(resultado<= 12)
+                Console.WriteLine("Comprar com Urgência");
+
+            else if(resultado<= 20)
+                Console.WriteLine("Realizar Compra");
+
+            else Console.WriteLine("Ponto de Utilização");
+        }
+    }
+}
